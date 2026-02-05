55b1f891aa3b94b4d64628352cc3d3ad28b912a5
Wilton Gonçalves <wilton.goncalves@fbter.org.br>
2026-02-05 10:35:51 -0300

    Alteração do título da página 'Sistema de Vagas' para 'Sistema de Oportunidades'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Sistema de Vagas')</title>
+    <title>@yield('title', 'Sistema de Oportunidades')</title>
     <link rel="stylesheet" href="{{ asset('assets/icons/bootstrap-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <style>
