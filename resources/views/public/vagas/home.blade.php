ec5813a86ba8e74728a2b441463dfe6346d780a8
Wilton Gonçalves <wilton.goncalves@fbter.org.br>
2026-02-05 10:34:47 -0300

    Alteração do título da página Vagas - SENAI CIMATEC para Oportunidades - SENAI CIMATEC

--- resources/views/public/vagas/home.blade.php
+++ resources/views/public/vagas/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vagas - SENAI CIMATEC</title>
+    <title>Oportunidades - SENAI CIMATEC</title>
     <link rel="stylesheet" href="{{ asset('assets/icons/bootstrap-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
