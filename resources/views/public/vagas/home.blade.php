37161208a599bddddca5fcb377b0ff4e7874824d
Wilton Gonçalves <wilton.goncalves@fbter.org.br>
2026-02-05 13:42:32 -0300

    Carregamento do favico na página

--- resources/views/public/vagas/home.blade.php
+++ resources/views/public/vagas/home.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oportunidades - SENAI CIMATEC</title>
+    <link rel="icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/ico">
     <link rel="stylesheet" href="{{ asset('assets/icons/bootstrap-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
